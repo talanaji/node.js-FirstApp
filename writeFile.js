4c1c01cf1c870dc4e54a7743944abc442d79eae1
talanaji <talanaji@gmail.com>
2021-04-28 17:59:55 +0200

    write /replace file if not exist create it

---
+++ writeFile.js
@@ -0,0 +1,6 @@
+var fs = require('fs');
+//The fs.writeFile() method replaces the specified file and content if it exists. If the file does not exist, a new file, containing the specified content, will be created
+fs.writeFile('mynewfile3.txt', 'Hello content!', function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+});
