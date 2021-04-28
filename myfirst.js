9ee22863721945053d0f2b8024884840a16c11bd
talanaji <talanaji@gmail.com>
2021-04-28 17:22:08 +0200

    read the query string

--- myfirst.js
+++ myfirst.js
@@ -9,7 +9,7 @@ http.createServer(function (req, res) {
 //	define that the response from the HTTP server is supposed to be displayed as HTML, so we define HTTP header and give it the  status code and an object containing the response headers
   res.writeHead(200, {'Content-Type': 'text/html'});
 //try to use function of my module
-  res.write("The date and time are currently: " + dt.myDateTime());
-
+  res.write("The date and time are currently: " + dt.myDateTime()+"</br>");
+  res.write(req.url);
   res.end('Hello World!');
 }).listen(8080);
