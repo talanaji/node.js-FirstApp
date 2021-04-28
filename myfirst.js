cc2e1406f6135ec494bb44e237c4390ab9e5f605
talanaji <talanaji@gmail.com>
2021-04-28 16:49:10 +0200

    create my own module

--- myfirst.js
+++ myfirst.js
@@ -1,5 +1,7 @@
+// define HTTP module, and is able to create a server 
 var http = require('http');
 
+//define createServer function and set port number for server
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('Hello World!');
---
+++ myfirstmodule.js
@@ -0,0 +1,5 @@
+//try to  creates a module that returns a date and time object
+
+exports.myDateTime = function () {
+  return Date();
+};
