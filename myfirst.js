46cca2084ea420d1aea57251cb7f337680fe55d0
talanaji <talanaji@gmail.com>
2021-04-28 17:27:58 +0200

    Split the query string

--- myfirst.js
+++ myfirst.js
@@ -1,6 +1,9 @@
 // define HTTP module,allows Node.js to transfer data over the Hyper Text Transfer Protocol (HTTP).
 var http = require('http');
 
+//include url built-in module to split the query string 
+var url= require('url');
+
 // include my own module 
 var dt = require('./myfirstmodule');
 
@@ -8,8 +11,13 @@ var dt = require('./myfirstmodule');
 http.createServer(function (req, res) {
 //	define that the response from the HTTP server is supposed to be displayed as HTML, so we define HTTP header and give it the  status code and an object containing the response headers
   res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Hello World!');
 //try to use function of my module
   res.write("The date and time are currently: " + dt.myDateTime()+"</br>");
   res.write(req.url);
-  res.end('Hello World!');
+  res.write('</br>');
+  var q = url.parse(req.url, true).query;
+  var txt = q.year +" "+q.month; 
+  res.write(txt);
+  res.end();
 }).listen(8080);
