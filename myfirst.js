44a8d0aeddff92ddf36fc2c1dff4872b3266f35d
talanaji <talanaji@gmail.com>
2021-04-28 16:55:48 +0200

    include my own module

--- myfirst.js
+++ myfirst.js
@@ -1,8 +1,14 @@
 // define HTTP module, and is able to create a server 
 var http = require('http');
 
+// include my own module 
+var dt = require('./myfirstmodule');
+
 //define createServer function and set port number for server
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
+//try to use function of my module
+  res.write("The date and time are currently: " + dt.myDateTime());
+
   res.end('Hello World!');
 }).listen(8080);
